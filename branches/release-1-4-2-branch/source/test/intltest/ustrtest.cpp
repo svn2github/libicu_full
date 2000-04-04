f5ffa443a274556b9685c291bf2b2abc01260515
schererm <schererm@251d0590-4201-4cf1-90de-194747b24ca1>
2000-04-04 22:31:44 +0000

    ticket:315: more S/390 and EBCDIC changes
    git-svn-id: http://source.icu-project.org/repos/icu/icu@1062 251d0590-4201-4cf1-90de-194747b24ca1

--- branches/release-1-4-2-branch/source/test/intltest/ustrtest.cpp
+++ branches/release-1-4-2-branch/source/test/intltest/ustrtest.cpp
@@ -169,6 +169,9 @@ UnicodeStringTest::TestExtract()
     test1.extract(11, 12, test4);
     test1.extractBetween(11, 23, test5);
 
+    // convert test4 back to Unicode for comparison
+    UnicodeString test4b(test4, 12);
+
     for (UTextOffset i = 0; i < 12; i++) {
         if (test1[(UTextOffset)(11 + i)] != test2[i]) {
             errln(UnicodeString("extracting into a UnicodeString failed at position ") + i);
@@ -178,7 +181,7 @@ UnicodeStringTest::TestExtract()
             errln(UnicodeString("extracting into an array of UChar failed at position ") + i);
             break;
         }
-        if ((((char)test1[(UTextOffset)(11 + i)]) & 0xff) != test4[i]) {
+        if (((char)test1[(UTextOffset)(11 + i)]) != test4b[i]) {
             errln(UnicodeString("extracting into an array of char failed at position ") + i);
             break;
         }
