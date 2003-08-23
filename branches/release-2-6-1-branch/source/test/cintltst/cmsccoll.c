2d859b4dd29d70428d3b18457f83eded5c9a40f3
weiv-oss <weiv-oss@251d0590-4201-4cf1-90de-194747b24ca1>
2003-08-23 04:57:28 +0000

    ticket:3141: port UCA4.0 test updates to branch 2-6-1
    git-svn-id: http://source.icu-project.org/repos/icu/icu@12927 251d0590-4201-4cf1-90de-194747b24ca1

--- branches/release-2-6-1-branch/source/test/cintltst/cmsccoll.c
+++ branches/release-2-6-1-branch/source/test/cintltst/cmsccoll.c
@@ -3689,56 +3689,69 @@ static void TestRuleOptions(void) {
   } tests[] = {  
     /* - all befores here amount to zero */
     { "&[before 1][first tertiary ignorable]<<<a", 
-    { "\\u0000", "a"}, 2}, /* you cannot go before first tertiary ignorable */
+        { "\\u0000", "a"}, 2
+    }, /* you cannot go before first tertiary ignorable */
 
     { "&[before 1][last tertiary ignorable]<<<a", 
-    { "\\u0000", "a"}, 2}, /* you cannot go before last tertiary ignorable */
+        { "\\u0000", "a"}, 2
+    }, /* you cannot go before last tertiary ignorable */
 
     { "&[before 1][first secondary ignorable]<<<a", 
-    { "\\u0000", "a"}, 2}, /* you cannot go before first secondary ignorable */
+        { "\\u0000", "a"}, 2
+    }, /* you cannot go before first secondary ignorable */
 
     { "&[before 1][last secondary ignorable]<<<a", 
-    { "\\u0000", "a"}, 2}, /* you cannot go before first secondary ignorable */
+        { "\\u0000", "a"}, 2
+    }, /* you cannot go before first secondary ignorable */
 
     /* 'normal' befores */
 
     { "&[before 1][first primary ignorable]<<<c<<<b &[first primary ignorable]<a", 
-    {  "c", "b", "\\u0332", "a" }, 4},
+        {  "c", "b", "\\u0332", "a" }, 4
+    },
 
     /* we don't have a code point that corresponds to  
      * the last primary ignorable
      */
     { "&[before 2][last primary ignorable]<<<c<<<b &[last primary ignorable]<a", 
-    {  "\\u0332", "\\u20e3", "c", "b", "a" }, 5}, 
+        {  "\\u0332", "\\u20e3", "c", "b", "a" }, 5
+    }, 
 
     { "&[before 1][first variable]<<<c<<<b &[first variable]<a", 
-    {  "c", "b", "\\u0009", "a", "\\u000a" }, 5}, 
+        {  "c", "b", "\\u0009", "a", "\\u000a" }, 5
+    }, 
 
     { "&[last variable]<a &[before 1][last variable]<<<c<<<b ", 
-    {  "c", "b", "\\uD800\\uDF23", "a", "\\u02d0" }, 5}, 
+        {  "c", "b", "\\uD800\\uDD33", "a", "\\u02d0" }, 5
+    }, 
 
     { "&[first regular]<a"
       "&[before 1][first regular]<b",
-    { "b", "\\u02d0", "a", "\\u02d1"}, 4},
+      { "b", "\\u02d0", "a", "\\u02d1"}, 4
+    },
 
     { "&[before 1][last regular]<b"
       "&[last regular]<a",
-    { "b", "\\uD801\\uDC25", "a", "\\u4e00" }, 4},     
+        { "b", "\\uD800\\uDF9D", "a", "\\u4e00" }, 4
+    },     
 
     { "&[before 1][first implicit]<b"
       "&[first implicit]<a",
-    { "b", "\\u4e00", "a", "\\u4e01"}, 4},
+        { "b", "\\u4e00", "a", "\\u4e01"}, 4
+    },
 
     { "&[before 1][last implicit]<b"
       "&[last implicit]<a",
-    { "b", "\\U0010FFFC", "a" }, 3},     
+        { "b", "\\U0010FFFC", "a" }, 3
+    },     
 
     { "&[last variable]<z"
       "&[last primary ignorable]<x"
       "&[last secondary ignorable]<<y"
       "&[last tertiary ignorable]<<<w"
       "&[top]<u",
-      {"\\ufffb",  "w", "y", "\\u20e3", "x", "\\u137c", "z", "u"}, 7 }
+      {"\\ufffb",  "w", "y", "\\u20e3", "x", "\\u137c", "z", "u"}, 7 
+    }
 
   };
   uint32_t i;
@@ -4191,6 +4204,7 @@ static void TestJ2726(void) {
   doTest(coll, aSpace, spaceA, UCOL_EQUAL);
   ucol_close(coll);
 }
+
 
 
 #define TEST(x) addTest(root, &x, "tscoll/cmsccoll/" # x)
