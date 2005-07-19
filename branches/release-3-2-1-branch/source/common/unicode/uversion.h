a4727e25039d2e2c39a38486bacab628bb65cbbb
weiv <weiv@251d0590-4201-4cf1-90de-194747b24ca1>
2005-07-19 21:15:20 +0000

    ticket:4675: 3.2.1 branch
    git-svn-id: http://source.icu-project.org/repos/icu/icu@18293 251d0590-4201-4cf1-90de-194747b24ca1

--- branches/release-3-2-1-branch/source/common/unicode/uversion.h
+++ branches/release-3-2-1-branch/source/common/unicode/uversion.h
@@ -1,6 +1,6 @@
 /*
 *******************************************************************************
-*   Copyright (C) 2000-2004, International Business Machines
+*   Copyright (C) 2000-2005, International Business Machines
 *   Corporation and others.  All Rights Reserved.
 *******************************************************************************
 *
@@ -67,7 +67,7 @@
  *  This value will change in the subsequent releases of ICU
  *  @stable ICU 2.4
  */
-#define U_ICU_VERSION_PATCHLEVEL_NUM 0
+#define U_ICU_VERSION_PATCHLEVEL_NUM 1
 
 /** Glued version suffix for renamers 
  *  This value will change in the subsequent releases of ICU
@@ -80,7 +80,7 @@
  *  This value will change in the subsequent releases of ICU
  *  @stable ICU 2.4
  */
-#define U_ICU_VERSION "3.2"
+#define U_ICU_VERSION "3.2.1"
 
 /** The current ICU library major/minor version as a string without dots, for library name suffixes. 
  *  This value will change in the subsequent releases of ICU
