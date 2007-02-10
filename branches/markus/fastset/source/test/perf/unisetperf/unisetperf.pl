a828b2384133fe8db6c11732d958b77d8b24b086
schererm <schererm@251d0590-4201-4cf1-90de-194747b24ca1>
2007-02-10 00:03:23 +0000

    ticket:5585: initial work on unisetperf.pl
    git-svn-id: http://source.icu-project.org/repos/icu/icu@21030 251d0590-4201-4cf1-90de-194747b24ca1

--- branches/markus/fastset/source/test/perf/unisetperf/unisetperf.pl
+++ branches/markus/fastset/source/test/perf/unisetperf/unisetperf.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
 #  ********************************************************************
 #  * COPYRIGHT:
-#  * Copyright (c) 2005, International Business Machines Corporation and
+#  * Copyright (c) 2005-2007, International Business Machines Corporation and
 #  * others. All Rights Reserved.
 #  ********************************************************************
 
@@ -13,33 +13,40 @@ use PerfFramework;
 
 
 my $options = {
-	       "title"=>"Utf performance: ICU",
+	       "title"=>"UnicodeSet span()/contains() performance",
 	       "headers"=>"ICU",
-	       "operationIs"=>"gb18030 encoding string",
+	       "operationIs"=>"Unicode string",
 	       "passes"=>"1",
 	       "time"=>"2",
 	       #"outputType"=>"HTML",
-	       "dataDir"=>"../data",
+	       "dataDir"=>"/temp/udhr",
 	       "outputDir"=>"../results"
 	      };
 
 # programs
 # tests will be done for all the programs. Results will be stored and connected
-my $p = "debug/utfperf.exe -e gb18030";
+my $p = "Release/unisetperf.exe -e gb18030";
 
 my $tests = { 
-	     "UTF-8",  ["$p UTF_8"],
-	     "UTF-8 small buffer",  ["$p UTF_8_SB"],
-	     "SCSU",  ["$p SCSU"],
-	     "SCSU small buffer",  ["$p SCSU_SB"],
-	     "BOCU_1",  ["$p BOCU_1"],
-	     "BOCU_1 small buffer",  ["$p BOCU_1_SB"],
+	     "SpanUTF8  Bv",    ["$p SpanUTF8  --type Bv   -e UTF-8 "],
+	     "SpanUTF8  BvF",   ["$p SpanUTF8  --type BvF  -e UTF-8 "],
+	     "SpanUTF8  Bvp",   ["$p SpanUTF8  --type Bvp  -e UTF-8 "],
+	     "SpanUTF8  BvpF",  ["$p SpanUTF8  --type BvpF -e UTF-8 "],
+	     "SpanUTF8  L",     ["$p SpanUTF8  --type L    -e UTF-8 "],
+	     "SpanUTF8  Bvl",   ["$p SpanUTF8  --type Bvl  -e UTF-8 "],
 	    };
 
 my $dataFiles = {
 		 "",
 		 [
-		  "four.txt"
+		  "udhr_eng.txt",
+          "udhr_deu.txt",
+          "udhr_fra.txt",
+          "udhr_rus.txt",
+          "udhr_tha.txt",
+          "udhr_jpn.txt",
+          "udhr_cmn.txt",
+          "udhr_jpn.html"
 		 ]
 		};
 
