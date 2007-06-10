38cc2d68f543523c92a2e9bddf00be1985ff9224
emmons <emmons@251d0590-4201-4cf1-90de-194747b24ca1>
2007-06-10 03:19:32 +0000

    ticket:5486: CLDR update 6-9
    git-svn-id: http://source.icu-project.org/repos/icu/icu@21683 251d0590-4201-4cf1-90de-194747b24ca1

--- branches/emmons/cldr15/source/data/coll/colfiles.mk
+++ branches/emmons/cldr15/source/data/coll/colfiles.mk
@@ -69,17 +69,17 @@ COLLATION_EMPTY_SOURCE = ar_AE.txt ar_BH.txt ar_DZ.txt ar_EG.txt\
 
 # Ordinary resources
 COLLATION_SOURCE = $(COLLATION_EMPTY_SOURCE) ar.txt as.txt be.txt bg.txt\
- bn.txt bo.txt ca.txt cs.txt da.txt\
- de.txt dz.txt el.txt en.txt en_BE.txt\
- eo.txt es.txt et.txt fa.txt fa_AF.txt\
- fi.txt fo.txt fr.txt gu.txt haw.txt\
- he.txt hi.txt hr.txt hu.txt is.txt\
- it.txt ja.txt kk.txt kl.txt km.txt\
- kn.txt ko.txt kok.txt lt.txt lv.txt\
- mk.txt ml.txt mr.txt mt.txt nb.txt\
- nn.txt om.txt or.txt pa.txt pl.txt\
- ps.txt ro.txt ru.txt sa.txt sk.txt\
- sl.txt sq.txt sr.txt sv.txt ta.txt\
- te.txt th.txt tr.txt uk.txt vi.txt\
- zh.txt zh_Hant.txt
+ bn.txt ca.txt cs.txt da.txt de.txt\
+ dz.txt el.txt en.txt en_BE.txt eo.txt\
+ es.txt et.txt fa.txt fa_AF.txt fi.txt\
+ fo.txt fr.txt gu.txt haw.txt he.txt\
+ hi.txt hr.txt hu.txt is.txt it.txt\
+ ja.txt kk.txt kl.txt km.txt kn.txt\
+ ko.txt kok.txt lt.txt lv.txt mk.txt\
+ ml.txt mr.txt mt.txt nb.txt nn.txt\
+ om.txt or.txt pa.txt pl.txt ps.txt\
+ ro.txt ru.txt sa.txt sk.txt sl.txt\
+ sq.txt sr.txt sv.txt ta.txt te.txt\
+ th.txt tr.txt uk.txt vi.txt zh.txt\
+ zh_Hant.txt
 
