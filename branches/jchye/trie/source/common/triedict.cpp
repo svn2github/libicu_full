2b61318839cdb0f2a2562ae42787308c6524096a
jchye <jchye@251d0590-4201-4cf1-90de-194747b24ca1>
2007-10-11 23:24:17 +0000

    ticket:2229: test checkin
    git-svn-id: http://source.icu-project.org/repos/icu/icu@22778 251d0590-4201-4cf1-90de-194747b24ca1

--- branches/jchye/trie/source/common/triedict.cpp
+++ branches/jchye/trie/source/common/triedict.cpp
@@ -1,4 +1,4 @@
-/**
+/**  
  *******************************************************************************
  * Copyright (C) 2006-2007, International Business Machines Corporation        *
  * and others. All Rights Reserved.                                            *
