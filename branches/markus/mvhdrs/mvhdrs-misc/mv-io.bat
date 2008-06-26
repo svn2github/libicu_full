205761042e64ac303b2ff47c425432dca186baca
schererm <schererm@251d0590-4201-4cf1-90de-194747b24ca1>
2008-06-26 08:48:19 +0000

    ticket:5943: add scripts and notes
    git-svn-id: http://source.icu-project.org/repos/icu/icu@24298 251d0590-4201-4cf1-90de-194747b24ca1

---
+++ branches/markus/mvhdrs/mvhdrs-misc/mv-io.bat
@@ -0,0 +1,3 @@
+REM Copyright (C) 2008, International Business Machines Corporation and others. All Rights Reserved.
+svn mv source\io\unicode\ustdio.h include\unicode
+svn mv source\io\unicode\ustream.h include\unicode
---
+++ branches/markus/mvhdrs/mvhdrs-misc/mv-layout.bat
@@ -0,0 +1,15 @@
+REM Copyright (C) 2008, International Business Machines Corporation and others. All Rights Reserved.
+svn mv source\layout\LayoutEngine.h include\layout
+svn mv source\layout\LEFontInstance.h include\layout
+svn mv source\layout\LEGlyphFilter.h include\layout
+svn mv source\layout\LEGlyphStorage.h include\layout
+svn mv source\layout\LEInsertionList.h include\layout
+svn mv source\layout\LELanguages.h include\layout
+svn mv source\layout\LEScripts.h include\layout
+svn mv source\layout\LESwaps.h include\layout
+svn mv source\layout\LETypes.h include\layout
+svn mv source\layout\loengine.h include\layout
+svn mv source\layoutex\layout\ParagraphLayout.h include\layout
+svn mv source\layoutex\layout\playout.h include\layout
+svn mv source\layoutex\layout\plruns.h include\layout
+svn mv source\layoutex\layout\RunArrays.h include\layout
