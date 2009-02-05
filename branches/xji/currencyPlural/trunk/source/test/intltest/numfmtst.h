c39ff141f1a24be41d27396f072d91357d98928a
xji <xji@251d0590-4201-4cf1-90de-194747b24ca1>
2009-02-05 19:16:46 +0000

    ticket:6433: currency plural format
    git-svn-id: http://source.icu-project.org/repos/icu/icu@25379 251d0590-4201-4cf1-90de-194747b24ca1

--- branches/xji/currencyPlural/trunk/source/test/intltest/numfmtst.h
+++ branches/xji/currencyPlural/trunk/source/test/intltest/numfmtst.h
@@ -1,6 +1,6 @@
 /********************************************************************
  * COPYRIGHT:
- * Copyright (c) 1997-2008, International Business Machines Corporation and
+ * Copyright (c) 1997-2009, International Business Machines Corporation and
  * others. All Rights Reserved.
  ********************************************************************/
 
@@ -131,6 +131,13 @@ class NumberFormatTest: public CalendarTimeZoneTest {
     void TestRounding(void);
 
     void TestNonpositiveMultiplier(void);
+
+    void TestSpaceParsing();
+    void TestMultiCurrencySign();
+    void TestCurrencyFormatForMixParsing();
+    void TestDecimalFormatCurrencyParse();
+    void TestCurrencyIsoPluralFormat();
+
  private:
 
     static UBool equalValue(const Formattable& a, const Formattable& b);
