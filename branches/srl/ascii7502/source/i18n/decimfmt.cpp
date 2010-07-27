0d9fa46420310fc677df3123de10ced88d1670dc
srl <srl@251d0590-4201-4cf1-90de-194747b24ca1>
2010-07-27 00:38:19 +0000

    ticket:7502: intltests pass! fixed more asciisms
    git-svn-id: http://source.icu-project.org/repos/icu/icu@28372 251d0590-4201-4cf1-90de-194747b24ca1

--- branches/srl/ascii7502/source/i18n/decimfmt.cpp
+++ branches/srl/ascii7502/source/i18n/decimfmt.cpp
@@ -3414,7 +3414,7 @@ DecimalFormat::toPattern(UnicodeString& result, UBool localized) const
     }
     if (fRoundingIncrement != NULL) {
         for(i=0; i<fRoundingIncrement->getCount(); ++i) {
-            roundingDigits.append((UChar)fRoundingIncrement->getDigit(i));
+          roundingDigits.append(kPatternZeroDigit+(fRoundingIncrement->getDigit(i)-'0')); // Convert to Unicode digit
         }
         roundingDecimalPos = fRoundingIncrement->getDecimalAt();
     }
