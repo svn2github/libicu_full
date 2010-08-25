b15e27c5960d82b2b3fbdf7ea4df888176a4b9b7
agousev <agousev@251d0590-4201-4cf1-90de-194747b24ca1>
2010-08-25 18:45:47 +0000

    ticket:7066: updating diffs
    git-svn-id: http://source.icu-project.org/repos/icu/icu@28526 251d0590-4201-4cf1-90de-194747b24ca1

--- branches/agousev/patch7066/source/io/unicode/ustream.h
+++ branches/agousev/patch7066/source/io/unicode/ustream.h
@@ -11,7 +11,7 @@
 *   06/25/2001  grhoten     Move iostream from unistr.h
 ******************************************************************************
 */
-   
+
 #ifndef USTREAM_H
 #define USTREAM_H
 
@@ -27,8 +27,12 @@
  */
 
 #if U_IOSTREAM_SOURCE >= 199711
+#if (__GNUC__ == 2)
+#include <iostream>
+#else
 #include <istream>
 #include <ostream>
+#endif
 
 U_NAMESPACE_BEGIN
 
