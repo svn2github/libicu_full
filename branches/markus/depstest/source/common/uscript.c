9f3498db8161a1206f2e33b203041ade8e41c554
schererm <schererm@251d0590-4201-4cf1-90de-194747b24ca1>
2011-05-31 00:23:33 +0000

    ticket:8605: move uscript_getName() & uscript_getShortName() to propname.cpp: they are just thin convenience wrappers
    git-svn-id: http://source.icu-project.org/repos/icu/icu@30170 251d0590-4201-4cf1-90de-194747b24ca1

--- branches/markus/depstest/source/common/uscript.c
+++ branches/markus/depstest/source/common/uscript.c
@@ -95,16 +95,3 @@ uscript_getCode(const char* nameOrAbbrOrLocale,
     }
     return numFilled;
 }
-
-U_CAPI const char*  U_EXPORT2
-uscript_getName(UScriptCode scriptCode){
-    return u_getPropertyValueName(UCHAR_SCRIPT, scriptCode,
-                                  U_LONG_PROPERTY_NAME);
-}
-
-U_CAPI const char*  U_EXPORT2
-uscript_getShortName(UScriptCode scriptCode){
-    return u_getPropertyValueName(UCHAR_SCRIPT, scriptCode,
-                                  U_SHORT_PROPERTY_NAME);
-}
-
