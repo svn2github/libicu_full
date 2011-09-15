06e3fb057b1c54a22f0c69d551d3270a72ff819c
srl <srl@251d0590-4201-4cf1-90de-194747b24ca1>
2011-09-15 00:26:53 +0000

    ticket:8784 various fixes for i
    git-svn-id: http://source.icu-project.org/repos/icu/icu@30658 251d0590-4201-4cf1-90de-194747b24ca1

--- branches/srl/i8784_m48/as_is/os400/iculd.c
+++ branches/srl/i8784_m48/as_is/os400/iculd.c
@@ -1,10 +1,21 @@
- /* Copyright (C) 2011 IBM Corporation and Others. All Rights Reserved */
+/* Copyright (C) 2011 IBM Corporation and Others. All Rights Reserved */
 
 /**
    Input:
        -o makeconv  makeconv.o ucnvstat.o ../../lib/libicuuc48.so -qOPTION='*DUPPROC *DUPVAR*'
 
-CRTPGM PGM(SRLICU/MAKECONV) MODULE(SRLICU/MAKECONV  SRLICU/UCNVSTAT SRLICU/GENMBCS SRLICU/GENCNVEX) BNDSRVPGM(SRLICU/LIBICUUC48 SRLICU/LIBICUTU48 SRLICU/LIBICUIN48) OPTION(*DUPPROC *DUPVAR) REPLACE(*YES) */
+CRTPGM PGM(SRLICU/MAKECONV) MODULE(SRLICU/MAKECONV  SRLICU/UCNVSTAT SRLICU/GENMBCS SRLICU/GENCNVEX) BNDSRVPGM(SRLICU/LIBICUUC48 SRLICU/LIBICUTU48 SRLICU/LIBICUIN48) OPTION(*DUPPROC *DUPVAR) REPLACE(*YES) 
+
+Handles  .o ( modules ), .so ( srvpgm ), .a ( bnddir ).
+
+TODO:
+
+ - cleanup
+ - much better error handling
+ - factor common code
+ - instead of caring about .o vs .so vs .a, just read the link - if it ends in .srvpgm then treat it as a service program, etc.  
+
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -48,7 +59,8 @@ int main(int argc, const char *argv[]) {
   char opt[4100];
   char objs[4024];
   char libs[4024];
-  const char *prog="icuprog";
+  char bnddirs[4024];
+  const char *prog="";
   const char *progshort=prog;
   const char *outputdir=getenv("OUTPUTDIR");
 
@@ -62,12 +74,16 @@ int main(int argc, const char *argv[]) {
   opt[0]=0;
   objs[0]=0;
   libs[0]=0;
+  bnddirs[0]=0;
 
   for(i=1;i<argc;i++) {
     if(argv[i][0]=='-') {
       switch(argv[i][1]) {
       case 'O':
         printf(".. ignoring optimization: %s\n", argv[i]);
+        break;
+      case 'g':
+        printf(".. ignoring debugging: %s\n", argv[i]);
         break;
       case 'v':
         printf(".. already verbose\n");
@@ -130,6 +146,40 @@ int main(int argc, const char *argv[]) {
         strcat(objs,"/");
         strncat(objs,b,nlen);
         strcat(objs, " ");
+      } else if(argv[i][n-1]=='a' &&
+         argv[i][n-2]=='.') {
+        const char *b = argv[i];
+        char linkbuf[200];
+        char outbuf[100];
+        int nlen = n-2;
+
+        if(nlen >= 10) {
+          nlen = 10;
+        }
+
+        if(readlink(b,linkbuf,200)>0) {
+          /* printf("linkbuf %s for %s\n", linkbuf, b); */
+          /* /qsys.lib/srlicu.lib/currtest.srvpgm */
+          char *mend = strrchr(linkbuf,'.');  
+          if(mend) {
+            *mend=0;
+            mend = strrchr(linkbuf,'/');
+            if(mend) {
+              mend++;
+              strcpy(outbuf,mend);
+              b=outbuf;
+              nlen=strlen(b);
+            }
+          }
+        } else {
+          /* perror("readlink");
+             puts(b); */
+        }
+
+        strcat(bnddirs,outputdir);
+        strcat(bnddirs,"/");
+        strncat(bnddirs,b,nlen);
+        strcat(bnddirs, " ");
       } else if(argv[i][n-1]=='o' &&
          argv[i][n-2]=='s' &&
          argv[i][n-3]=='.') {
@@ -149,12 +199,19 @@ int main(int argc, const char *argv[]) {
     }
   }
 
-  sprintf(buf,"CRTPGM PGM(%s/%s) MODULE(%s)  BNDSRVPGM(%s) OPTION(%s) REPLACE(*YES)",
+  if(prog[0]==0) {
+    printf("no program (-o) option specified.\n");
+    return 1;
+  }
+
+  sprintf(buf,"CRTPGM PGM(%s/%s) MODULE(%s) BNDSRVPGM(%s) BNDDIR(%s) OPTION(%s) REPLACE(*YES)",
           outputdir,progshort,
 
           objs,
 
           libs,
+
+          bnddirs,
 
           opt);
 
