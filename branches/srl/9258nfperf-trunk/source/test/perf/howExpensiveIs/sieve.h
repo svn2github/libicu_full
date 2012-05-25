17b96f5143154696770657caf3c95432ca8703e5
srl <srl@251d0590-4201-4cf1-90de-194747b24ca1>
2012-05-25 20:10:50 +0000

    ticket:9258 merge from trunk
    git-svn-id: http://source.icu-project.org/repos/icu/icu@31864 251d0590-4201-4cf1-90de-194747b24ca1

--- branches/srl/9258nfperf-trunk/source/test/perf/howExpensiveIs/sieve.h
+++ branches/srl/9258nfperf-trunk/source/test/perf/howExpensiveIs/sieve.h
@@ -1,6 +1,6 @@
 /*
  **********************************************************************
- * Copyright (c) 2011,International Business Machines
+ * Copyright (c) 2011-2012,International Business Machines
  * Corporation and others.  All Rights Reserved.
  **********************************************************************
  */
