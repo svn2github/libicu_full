e5dda15beefd41d3a8057c565dd5f7006260ec63
srl <srl@251d0590-4201-4cf1-90de-194747b24ca1>
2012-05-29 20:30:42 +0000

    ticket:9258 50-60% improvement when formatting integers via stringpiece
    git-svn-id: http://source.icu-project.org/repos/icu/icu@31872 251d0590-4201-4cf1-90de-194747b24ca1

--- branches/srl/9258nfperf-m49/source/i18n/decimfmt.cpp
+++ branches/srl/9258nfperf-m49/source/i18n/decimfmt.cpp
@@ -1044,24 +1044,41 @@ DecimalFormat::format(int64_t number,
                       UnicodeString& appendTo,
                       FieldPosition& fieldPosition) const
 {
-#if 1
+  FieldPositionOnlyHandler handler(fieldPosition);
+  return _format(number, appendTo, handler);
+}
 
-  const UnicodeString *posPrefix = fPosPrefixPattern;
-  const UnicodeString *posSuffix = fPosSuffixPattern;
-  const UnicodeString *negSuffix = fNegSuffixPattern;
-  // TODO: keep a fCanUseFastpathParse around.  Change it if any of these things change.
+UnicodeString&
+DecimalFormat::format(int64_t number,
+                      UnicodeString& appendTo,
+                      FieldPositionIterator* posIter,
+                      UErrorCode& status) const
+{
+    FieldPositionIteratorHandler handler(posIter, status);
+    return _format(number, appendTo, handler);
+}
 
+UnicodeString&
+DecimalFormat::_format(int64_t number,
+                       UnicodeString& appendTo,
+                       FieldPositionHandler& handler) const
+{
+    // Bottleneck function for formatting int64_t
+    UErrorCode status = U_ZERO_ERROR;
 
 #if UCONFIG_FORMAT_FASTPATHS_49
-    const DecimalFormatInternal &data = internalData(fReserved);
+  // const UnicodeString *posPrefix = fPosPrefixPattern;
+  // const UnicodeString *posSuffix = fPosSuffixPattern;
+  // const UnicodeString *negSuffix = fNegSuffixPattern;
 
-    
+  const DecimalFormatInternal &data = internalData(fReserved);
+
 #ifdef FMT_DEBUG
-    data.dump();
+  data.dump();
   printf("fastpath? [%d]\n", number);
 #endif
     
-    if( data.fFastpathStatus==kFastpathYES ) {
+  if( data.fFastpathStatus==kFastpathYES ) {
 
 #define kZero 0x0030
     const int32_t MAX_IDX = MAX_DIGITS+2;
@@ -1080,9 +1097,7 @@ DecimalFormat::format(int64_t number,
     } while (n > 0);
     
 
-    FieldPositionOnlyHandler handler(fieldPosition);
-
-    // Slide the number to the start of the output str
+        // Slide the number to the start of the output str
     U_ASSERT(destIdx >= 0);
     int32_t length = MAX_IDX - destIdx -1;
     //uprv_memmove(outputStr, outputStr+MAX_IDX-length, length);
@@ -1094,12 +1109,15 @@ DecimalFormat::format(int64_t number,
 #ifdef FMT_DEBUG
     printf("prependZero=%d, length=%d, minintdig=%d\n", prependZero, length, getMinimumIntegerDigits());
 #endif    
+    int32_t intBegin = appendTo.length();
+
     while((prependZero--)>0) {
       appendTo.append(0x0030); // '0'
     }
 
     appendTo.append(outputStr+destIdx, length);
-    fieldPosition.setEndIndex(appendTo.length());
+    handler.addAttribute(kIntegerField, intBegin, appendTo.length());
+
     int32_t suffixLen = appendAffix(appendTo, number, handler, number<0, FALSE);
 
     //outputStr[length]=0;
@@ -1114,37 +1132,7 @@ DecimalFormat::format(int64_t number,
   } // end fastpath
 #endif
 
-  FieldPositionOnlyHandler handler(fieldPosition);
-  // this is what unum_formatInt64 calls.
-  // copied from _format.
-    UErrorCode status = U_ZERO_ERROR;
-    DigitList digits;
-    digits.set(number);
-    _format(digits, appendTo, handler, status);
-    // No way to return status from here.
-    return appendTo;
-#else
-  FieldPositionOnlyHandler handler(fieldPosition);
-    return _format(number, appendTo, handler);
-#endif
-}
-
-UnicodeString&
-DecimalFormat::format(int64_t number,
-                      UnicodeString& appendTo,
-                      FieldPositionIterator* posIter,
-                      UErrorCode& status) const
-{
-    FieldPositionIteratorHandler handler(posIter, status);
-    return _format(number, appendTo, handler);
-}
-
-UnicodeString&
-DecimalFormat::_format(int64_t number,
-                       UnicodeString& appendTo,
-                       FieldPositionHandler& handler) const
-{
-    UErrorCode status = U_ZERO_ERROR;
+  // Else the slow way - via DigitList
     DigitList digits;
     digits.set(number);
     return _format(digits, appendTo, handler, status);
@@ -1207,6 +1195,47 @@ DecimalFormat::format(const StringPiece &number,
                       FieldPositionIterator *posIter,
                       UErrorCode &status) const
 {
+#if UCONFIG_FORMAT_FASTPATHS_49
+  // don't bother if the int64 path is not optimized
+  int32_t len    = number.length();
+
+  if(len>0&&len<10) { /* 10 or more digits may not be an int64 */
+    const char *data = number.data();
+    int64_t num = 0;
+    UBool neg = FALSE;
+    UBool ok = TRUE;
+    
+    int32_t start  = 0;
+
+    if(data[start]=='+') {
+      start++;
+    } else if(data[start]=='-') {
+      neg=TRUE;
+      start++;
+    }
+
+    int32_t place = 1; /* 1, 10, ... */
+    for(int32_t i=len-1;i>=start;i--) {
+      if(data[i]>='0'&&data[i]<='9') {
+        num+=place*(int64_t)(data[i]-'0');
+      } else {
+        ok=FALSE;
+        break;
+      }
+      place *= 10;
+    }
+
+    if(ok) {
+      if(neg) {
+        num = -num;// add minus bit
+      }
+      // format as int64_t
+      return format(num, toAppendTo, posIter, status);
+    }
+    // else fall through
+  }
+#endif
+
     DigitList   dnum;
     dnum.set(number, status);
     if (U_FAILURE(status)) {
