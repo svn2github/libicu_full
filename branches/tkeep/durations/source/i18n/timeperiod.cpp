807fc5c270f5b3b2f152161497dc248a7b577669
tkeep <tkeep@251d0590-4201-4cf1-90de-194747b24ca1>
2013-05-10 21:56:56 +0000

    ticket:10017: Introduce TimePeriod constructors.
    git-svn-id: http://source.icu-project.org/repos/icu/icu@33636 251d0590-4201-4cf1-90de-194747b24ca1

--- branches/tkeep/durations/source/i18n/timeperiod.cpp
+++ branches/tkeep/durations/source/i18n/timeperiod.cpp
@@ -5,6 +5,7 @@
  *******************************************************************************
  */
 
+#include <iostream>
 #include <math.h>
 #include "unicode/timeperiod.h"
 #include "putilimp.h"
@@ -13,51 +14,112 @@
 
 U_NAMESPACE_BEGIN
 
+TimePeriod::TimePeriod() : fSize(0) {
+    initTimeUnits();
+}
+    
+TimePeriod::TimePeriod(
+        const TimeUnitAmount * const *timeUnitAmounts,
+        int32_t length,
+        UErrorCode& status) : fSize(0) {
+    initTimeUnits();
+    if (U_FAILURE(status)) {
+        // Object is already bogus.
+        return;
+    }
+    for (int32_t i = 0; i < length; ++i) {
+        const TimeUnitAmount *tua = timeUnitAmounts[i];
+        int32_t idx = tua->getTimeUnitField();
+        if (fFields[idx] != NULL) {
+            status = U_ILLEGAL_ARGUMENT_ERROR;
+            makeBogus();
+            return;
+        }
+        fFields[idx] = (TimeUnitAmount *) tua->clone();
+        ++fSize;
+    }
+    validate(status);
+    if (U_FAILURE(status)) {
+        makeBogus();
+        return;
+    }
+}
+
 TimePeriod::TimePeriod(const TimePeriod& other) : fSize(other.fSize) {
-    for (int32_t i = 0; i < sizeof(fFields) / sizeof(fFields[0]); ++i) {
-        if (other.fFields[i] == NULL) {
+    copyTimeUnits(other.fFields);
+}
+
+TimePeriod& TimePeriod::operator=(const TimePeriod &rhs) {
+    freeTimeUnits();
+    fSize = rhs.fSize;
+    copyTimeUnits(rhs.fFields);
+    return *this;
+}
+
+TimePeriod::~TimePeriod() {
+    freeTimeUnits();
+}
+
+UBool TimePeriod::operator==(const TimePeriod& other) const {
+    // If same object, they are equal
+    if (this == &other) {
+        return TRUE;
+    }
+    int32_t fieldCount = sizeof(fFields) / sizeof(fFields[0]);
+    for (int32_t i = 0; i < fieldCount; ++i) {
+        // both NULL
+        if (fFields[i] == NULL && other.fFields[i] == NULL) {
+            continue;
+        }    
+        // One is NULL, the other isn't.
+        if (fFields[i] == NULL || other.fFields[i] == NULL) {
+            return FALSE;
+        }
+        if (*fFields[i] != *other.fFields[i]) {
+            return FALSE;
+        }
+    }
+    return TRUE;
+}
+    
+UBool TimePeriod::operator!=(const TimePeriod& other) const {
+    return !(*this == other);
+}
+
+const TimeUnitAmount *TimePeriod::getAmount(
+        TimeUnit::UTimeUnitFields field) const {
+    return fFields[field];
+}
+
+void TimePeriod::makeBogus() {
+    freeTimeUnits();
+    initTimeUnits();
+    fSize = 0;
+}
+
+void TimePeriod::copyTimeUnits(const TimeUnitAmount* const *fields) {
+    int32_t fieldCount = sizeof(fFields) / sizeof(fFields[0]);
+    for (int32_t i = 0; i < fieldCount; ++i) {
+        if (fields[i] == NULL) {
             fFields[i] = NULL;
         } else {
-            fFields[i] = (TimeUnitAmount *) other.fFields[i]->clone();
+            fFields[i] = (TimeUnitAmount *) fields[i]->clone();
         }
     }
 }
 
-
-TimePeriod* TimePeriod::forAmounts(
-        const TimeUnitAmount * const *timeUnitAmounts,
-        int32_t length,
-        UErrorCode& status) {
-    if (U_FAILURE(status)) {
-        return NULL;
-    }
-    TimePeriod *result = new TimePeriod;
-    if (result == NULL) {
-        status = U_MEMORY_ALLOCATION_ERROR;
-        return NULL;
-    }
-    int32_t fieldCount = sizeof(result->fFields) / sizeof(result->fFields[0]);
+void TimePeriod::initTimeUnits() {
+    int32_t fieldCount = sizeof(fFields) / sizeof(fFields[0]);
     for (int32_t i = 0; i < fieldCount; ++i) {
-        result->fFields[i] = NULL;
+        fFields[i] = NULL;
     }
-    result->fSize = 0;
-    for (int32_t i = 0; i < length; ++i) {
-        const TimeUnitAmount *tua = timeUnitAmounts[i];
-        int32_t idx = tua->getTimeUnitField();
-        if (result->fFields[idx] != NULL) {
-            status = U_ILLEGAL_ARGUMENT_ERROR;
-            delete result;
-            return NULL;
-        }
-        result->fFields[idx] = (TimeUnitAmount *) tua->clone();
-        ++(result->fSize);
+}
+
+void TimePeriod::freeTimeUnits() {
+    int32_t fieldCount = sizeof(fFields) / sizeof(fFields[0]);
+    for (int32_t i = 0; i < fieldCount; ++i) {
+        delete fFields[i];
     }
-    result->validate(status);
-    if (U_FAILURE(status)) {
-        delete result;
-        return NULL;
-    }
-    return result;
 }
 
 void TimePeriod::validate(UErrorCode& status) const {
@@ -84,43 +146,6 @@ void TimePeriod::validate(UErrorCode& status) const {
             fractionalFieldEncountered = TRUE;
         }
     }
-}
-
-TimePeriod::~TimePeriod() {
-    int32_t fieldCount = sizeof(fFields) / sizeof(fFields[0]);
-    for (int32_t i = 0; i < fieldCount; ++i) {
-        delete fFields[i];
-    }
-}
-
-UBool TimePeriod::operator==(const TimePeriod& other) const {
-    // If same object, they are equal
-    if (this == &other) {
-        return TRUE;
-    }
-    int32_t fieldCount = sizeof(fFields) / sizeof(fFields[0]);
-    for (int32_t i = 0; i < fieldCount; ++i) {
-        if (fFields[i] == other.fFields[i]) {
-            continue;
-        }    
-        // One is NULL, the other isn't.
-        if (fFields[i] == NULL || other.fFields[i] == NULL) {
-            return FALSE;
-        }
-        if (*fFields[i] != *other.fFields[i]) {
-            return FALSE;
-        }
-    }
-    return TRUE;
-}
-    
-UBool TimePeriod::operator!=(const TimePeriod& other) const {
-    return !(*this == other);
-}
-
-const TimeUnitAmount *TimePeriod::getAmount(
-        TimeUnit::UTimeUnitFields field) const {
-    return fFields[field];
 }
 
 U_NAMESPACE_END
