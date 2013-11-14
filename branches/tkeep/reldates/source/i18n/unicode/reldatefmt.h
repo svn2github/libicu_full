08e98799c5f8ada739ca7194a146b76fc0505a29
tkeep <tkeep@251d0590-4201-4cf1-90de-194747b24ca1>
2013-11-14 00:08:02 +0000

    ticket:8464: Final changes for approved C++ API proposal.
    git-svn-id: http://source.icu-project.org/repos/icu/icu@34659 251d0590-4201-4cf1-90de-194747b24ca1

--- branches/tkeep/reldates/source/i18n/unicode/reldatefmt.h
+++ branches/tkeep/reldates/source/i18n/unicode/reldatefmt.h
@@ -1,6 +1,7 @@
 /*
 ********************************************************************************
-* Copyright (C) 1997-2013, International Business Machines Corporation and others.
+* Copyright (C) 1997-2013, International Business Machines Corporation and
+* others.
 * All Rights Reserved.
 ********************************************************************************
 *
@@ -14,133 +15,175 @@
 #include "unicode/utypes.h"
 #include "unicode/locid.h"
 
-
 /**
- * TODO (tkeep): Copy comments from JAVA once approved.
+ * Represents the unit for formatting a relative date. e.g "in 5 days"
+ * or "in 3 months"
+ * @draft ICU 53
  */
 typedef enum UDateRelativeUnit {
 
     /**
+     * Seconds
      * @draft ICU 53
      */
     UDAT_RELATIVE_SECONDS,
 
     /**
+     * Minutes
      * @draft ICU 53
      */
     UDAT_RELATIVE_MINUTES,
 
     /**
+     * Hours
      * @draft ICU 53
      */
     UDAT_RELATIVE_HOURS,
 
     /**
+     * Days
      * @draft ICU 53
      */
     UDAT_RELATIVE_DAYS,
 
     /**
+     * Weeks
      * @draft ICU 53
      */
     UDAT_RELATIVE_WEEKS,
 
     /**
+     * Months
      * @draft ICU 53
      */
     UDAT_RELATIVE_MONTHS, 
 
     /**
+     * Years
      * @draft ICU 53
      */
     UDAT_RELATIVE_YEARS, 
 } UDateRelativeUnit;
 
-
 /**
- * TODO (tkeep): Copy comments from JAVA once approved.
+ * Represents an absolute unit.
+ * @draft ICU 53
  */
 typedef enum UDateAbsoluteUnit {
 
     /**
+     * Sunday
      * @draft ICU 53
      */
     UDAT_ABSOLUTE_SUNDAY,
 
     /**
+     * Monday
      * @draft ICU 53
      */
     UDAT_ABSOLUTE_MONDAY,
 
     /**
+     * Tuesday
      * @draft ICU 53
      */
     UDAT_ABSOLUTE_TUESDAY,
 
     /**
+     * Wednesday
      * @draft ICU 53
      */
     UDAT_ABSOLUTE_WEDNESDAY,
 
     /**
+     * Thursday
      * @draft ICU 53
      */
     UDAT_ABSOLUTE_THURSDAY,
 
     /**
+     * Friday
      * @draft ICU 53
      */
     UDAT_ABSOLUTE_FRIDAY,
 
     /**
+     * Saturday
      * @draft ICU 53
      */
     UDAT_ABSOLUTE_SATURDAY,
 
     /**
+     * Day
      * @draft ICU 53
      */
     UDAT_ABSOLUTE_DAY,
 
     /**
+     * Week
      * @draft ICU 53
      */
     UDAT_ABSOLUTE_WEEK,
 
     /**
+     * Month
      * @draft ICU 53
      */
     UDAT_ABSOLUTE_MONTH,
 
     /**
+     * Year
      * @draft ICU 53
      */
     UDAT_ABSOLUTE_YEAR,
 
+    /**
+     * Now
+     * @draft ICU 53
+     */
+    UDAT_ABSOLUTE_NOW,
+
 } UDateAbsoluteUnit;
 
-
 /**
- * TODO (tkeep): Copy comments from JAVA once approved.
+ * Represents a direction for an absolute unit e.g "Next Tuesday"
+ * or "Last Tuesday"
+ * @draft ICU 53
  */
 typedef enum UDateDirection {
 
     /**
+     * Two before. Not fully supported in every locale.
+     * @draft ICU 53
+     */
+    UDAT_DIRECTION_LAST_2,
+
+    /**
+     * Last
      * @draft ICU 53
      */
     UDAT_DIRECTION_LAST,
 
     /**
+     * This
      * @draft ICU 53
      */
     UDAT_DIRECTION_THIS,
 
     /**
+     * Next
      * @draft ICU 53
      */
     UDAT_DIRECTION_NEXT,
 
     /**
+     * Two after. Not fully supported in every locale.
+     * @draft ICU 53
+     */
+    UDAT_DIRECTION_NEXT_2,
+
+    /**
+     * Plain, which means the absence of a qualifier.
      * @draft ICU 53
      */
     UDAT_DIRECTION_PLAIN,
@@ -153,140 +196,71 @@ U_NAMESPACE_BEGIN
 class NumberFormat;
 
 /**
- * TODO (tkeep): Copy class level comments from JAVA once they
- * are approved.
-
+ * Formats simple relative dates. There are two types of relative dates that
+ * it handles:
+ * <ul>
+ *   <li>relative dates with a quantity e.g "in 5 days"</li>
+ *   <li>relative dates without a quantity e.g "next Tuesday"</li>
+ * </ul>
+ * <p>
+ * This API is very basic and is intended to be a building block for more
+ * fancy APIs. The caller tells it exactly what to display in a locale
+ * independent way. While this class automatically provides the correct plural
+ * forms, the grammatical form is otherwise as neutral as possible. It is the
+ * caller's responsibility to handle cut-off logic such as deciding between
+ * displaying "in 7 days" or "in 1 week." This API supports relative dates
+ * involving one single unit. This API does not support relative dates
+ * involving compound units.
+ * e.g "in 5 days and 4 hours" nor does it support parsing.
+ * This class is NOT thread-safe.
+ * <p>
+ * Here are some examples of use:
+ * <blockquote>
+ * <pre>
+ * UErrorCode status = U_ZERO_ERROR;
+ * UnicodeString appendTo;
+ * RelativeDateTimeFormatter fmt(status);
+ * // Appends "in 1 day"
+ * fmt.format(
+ *     1, UDAT_DIRECTION_NEXT, UDAT_RELATIVE_DAYS, appendTo, status);
+ * // Appends "in 3 days"
+ * fmt.format(
+ *     3, UDAT_DIRECTION_NEXT, UDAT_RELATIVE_DAYS, appendTo, status);
+ * // Appends "3.2 years ago"
+ * fmt.format(
+ *     3.2, UDAT_DIRECTION_LAST, UDAT_RELATIVE_YEARS, appendTo, status);
+ * // Appends "last Sunday"
+ * fmt.format(UDAT_DIRECTION_LAST, UDAT_ABSOLUTE_SUNDAY, appendTo, status);
+ * // Appends "this Sunday"
+ * fmt.format(UDAT_DIRECTION_THIS, UDAT_ABSOLUTE_SUNDAY, appendTo, status);
+ * // Appends "next Sunday"
+ * fmt.format(UDAT_DIRECTION_NEXT, UDAT_ABSOLUTE_SUNDAY, appendTo, status);
+ * // Appends "Sunday"
+ * fmt.format(UDAT_DIRECTION_PLAIN, UDAT_ABSOLUTE_SUNDAY, appendTo, status);
+ * 
+ * // Appends "yesterday"
+ * fmt.format(UDAT_DIRECTION_LAST, UDAT_ABSOLUTE_DAY, appendTo, status);
+ * // Appends "today"
+ * fmt.format(UDAT_DIRECTION_THIS, UDAT_ABSOLUTE_DAY, appendTo, status);
+ * // Appends "tomorrow"
+ * fmt.format(UDAT_DIRECTION_NEXT, UDAT_ABSOLUTE_DAY, appendTo, status);
+ * // Appends "now"
+ * fmt.format(UDAT_DIRECTION_PLAIN, UDAT_ABSOLUTE_NOW, appendTo, status);
+ *
+ * </pre>
+ * </blockquote>
+ * <p>
+ * In the future, we may add more forms, such as abbreviated/short forms
+ * (3 secs ago), and relative day periods ("yesterday afternoon"), etc.
+ *
  * The RelativeDateTimeFormatter class is not intended for public subclassing.
-*/
+ * 
+ * @draft ICU 53
+ */
+
+
 class U_I18N_API RelativeDateTimeFormatter : public UObject {
 public:
-
-    enum RelativeUnit {
-
-        /**
-         * @draft ICU 53
-         */
-        kSeconds,
-
-        /**
-         * @draft ICU 53
-         */
-        kMinutes,
-
-        /**
-         * @draft ICU 53
-         */
-        kHours,
-
-        /**
-         * @draft ICU 53
-         */
-        kDays,
-
-        /**
-         * @draft ICU 53
-         */
-        kWeeks,
-
-        /**
-         * @draft ICU 53
-         */
-        kMonths,
-
-        /**
-         * @draft ICU 53
-         */
-        kYears
-    };
-
-    enum AbsoluteUnit {
-
-        /**
-         * @draft ICU 53
-         */
-        kSunday,
-
-        /**
-         * @draft ICU 53
-         */
-        kMonday,
-
-        /**
-         * @draft ICU 53
-         */
-        kTuesday,
-
-        /**
-         * @draft ICU 53
-         */
-        kWednesday,
-
-        /**
-         * @draft ICU 53
-         */
-        kThursday,
-
-        /**
-         * @draft ICU 53
-         */
-        kFriday,
-
-        /**
-         * @draft ICU 53
-         */
-        kSaturday,
-
-        /**
-         * @draft ICU 53
-         */
-        kDay,
-
-        /**
-         * @draft ICU 53
-         */
-        kWeek,
-
-        /**
-         * @draft ICU 53
-         */
-        kMonth,
-
-        /**
-         * @draft ICU 53
-         */
-        kYear,
-
-        /**
-         * @draft ICU 53
-         */
-        kNow
-    };
-
-    /**
-     * TODO (tkeep): Copy comments from JAVA once approved.
-     */
-    enum Direction {
-
-        /**
-         * @draft ICU 53
-         */
-        kLast,
-
-        /**
-         * @draft ICU 53
-         */
-        kThis,
-
-        /**
-         * @draft ICU 53
-         */
-        kNext,
-
-        /**
-         * @draft ICU 53
-         */
-        kPlain
-    };
 
     /**
      * Create RelativeDateTimeFormatter with default locale.
@@ -321,8 +295,14 @@ public:
 
 
     /**
-     * TODO(tkeep): Copy comment from JAVA once approved.
-     *
+     * Formats a relative date with a quantity such as "in 5 days" or
+     * "3 months ago"
+     * @param quantity The numerical amount e.g 5. This value is formatted
+     * according to this object's NumberFormat object.
+     * @param direction NEXT means a future relative date; LAST means a past
+     * relative date. If direction is anything else, this method sets
+     * status to U_ILLEGAL_ARGUMENT_ERROR.
+     * @param unit the unit e.g day? month? year?
      * @param appendTo The string to which the formatted result will be
      *  appended
      * @param status ICU error code returned here.
@@ -332,10 +312,13 @@ public:
     UnicodeString& format(double quantity, UDateDirection direction, UDateRelativeUnit unit, UnicodeString& appendTo, UErrorCode& status) const;
 
     /**
-     * TODO(tkeep): Copy comment from JAVA once approved.
-     *
+     * Formats a relative date without a quantity.
+     * @param direction NEXT, LAST, THIS, etc.
+     * @param unit e.g SATURDAY, DAY, MONTH
      * @param appendTo The string to which the formatted result will be
-     *  appended
+     *  appended. If the value of direction is documented as not being fully
+     *  supported in all locales then this method leaves appendTo unchanged if
+     *  no format string is available.
      * @param status ICU error code returned here.
      * @return appendTo
      * @draft ICU 53
@@ -343,7 +326,9 @@ public:
     UnicodeString& format(UDateDirection direction, UDateAbsoluteUnit unit, UnicodeString& appendTo, UErrorCode& status) const;
 
     /**
-     * TODO(tkeep): Copy comment from JAVA once approved.
+     * Combines a relative date string and a time string in this object's
+     * locale. This is done with the same date-time separator used for the
+     * default calendar in this locale.
      *
      * @param relativeDateString the relative date, e.g 'yesterday'
      * @param timeString the time e.g '3:45'
@@ -357,9 +342,11 @@ public:
         UnicodeString& appendTo, UErrorCode& status) const;
 
     /**
-     * TODO(tkeep): Copy comment from JAVA once approved.
-     *
+     * Specify which NumberFormat object this object should use for
+     * formatting numbers. By default this object uses the default
+     * NumberFormat object for this object's locale.
      * @param nf the NumberFormat object to use.
+     * @see #format(double, Direction, RelativeUnit)
      * @draft ICU 53
      */
     void setNumberFormat(const NumberFormat& nf);
