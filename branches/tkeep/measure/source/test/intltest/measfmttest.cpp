104c89ef7fda902b0ea98ac546364dce7c021418
tkeep <tkeep@251d0590-4201-4cf1-90de-194747b24ca1>
2014-01-09 21:31:06 +0000

    ticket:10268: Added MeasureFormatTest
    git-svn-id: http://source.icu-project.org/repos/icu/icu@34848 251d0590-4201-4cf1-90de-194747b24ca1

---
+++ branches/tkeep/measure/source/test/intltest/measfmttest.cpp
@@ -0,0 +1,50 @@
+/*
+*******************************************************************************
+* Copyright (C) 2014, International Business Machines Corporation and         *
+* others. All Rights Reserved.                                                *
+*******************************************************************************
+*
+* File MEASFMTTEST.CPP
+*
+*******************************************************************************
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "intltest.h"
+
+#if !UCONFIG_NO_FORMATTING
+
+#include "unicode/measfmt.h"
+
+#define LENGTHOF(array) (int32_t)(sizeof(array) / sizeof((array)[0]))
+
+class MeasureFormatTest : public IntlTest {
+public:
+    MeasureFormatTest() {
+    }
+
+    void runIndexedTest(int32_t index, UBool exec, const char *&name, char *par=0);
+private:
+    void TestGetAvailable();
+};
+
+void MeasureFormatTest::runIndexedTest(
+        int32_t index, UBool exec, const char *&name, char *) {
+    if (exec) {
+        logln("TestSuite MeasureFormatTest: ");
+    }
+    TESTCASE_AUTO_BEGIN;
+    TESTCASE_AUTO(TestGetAvailable);
+    TESTCASE_AUTO_END;
+}
+
+void MeasureFormatTest::TestGetAvailable() {
+}
+
+extern IntlTest *createMeasureFormatTest() {
+    return new MeasureFormatTest();
+}
+
+#endif
+
