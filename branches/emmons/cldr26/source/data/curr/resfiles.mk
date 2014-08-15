3fb3889750bb2f5846e3767283d8b2e3ac5d0da7
emmons <emmons@251d0590-4201-4cf1-90de-194747b24ca1>
2014-08-15 05:21:45 +0000

    ticket:11056: Update with CLDR data through r10821
    git-svn-id: http://source.icu-project.org/repos/icu/icu@36172 251d0590-4201-4cf1-90de-194747b24ca1

--- branches/emmons/cldr26/source/data/curr/resfiles.mk
+++ branches/emmons/cldr26/source/data/curr/resfiles.mk
@@ -37,12 +37,12 @@ CURR_SYNTHETIC_ALIAS = az_AZ.txt az_Latn_AZ.txt bs_BA.txt bs_Latn_BA.txt\
  pa_Guru_IN.txt pa_IN.txt pa_PK.txt sh.txt sh_BA.txt\
  sh_CS.txt sh_YU.txt shi_MA.txt shi_Tfng_MA.txt sr_BA.txt\
  sr_CS.txt sr_Cyrl_CS.txt sr_Cyrl_RS.txt sr_Cyrl_XK.txt sr_Cyrl_YU.txt\
- sr_Latn_BA.txt sr_Latn_CS.txt sr_Latn_ME.txt sr_Latn_RS.txt sr_Latn_YU.txt\
- sr_ME.txt sr_RS.txt sr_XK.txt sr_YU.txt th_TH.txt\
- th_TH_TRADITIONAL.txt tl.txt tl_PH.txt tzm_Latn_MA.txt tzm_MA.txt\
- ug_Arab_CN.txt ug_CN.txt uz_AF.txt uz_Arab_AF.txt uz_Latn_UZ.txt\
- uz_UZ.txt vai_LR.txt vai_Vaii_LR.txt zh_CN.txt zh_HK.txt\
- zh_Hans_CN.txt zh_Hant_TW.txt zh_MO.txt zh_SG.txt zh_TW.txt
+ sr_Latn_CS.txt sr_Latn_ME.txt sr_Latn_RS.txt sr_Latn_YU.txt sr_ME.txt\
+ sr_RS.txt sr_XK.txt sr_YU.txt th_TH.txt th_TH_TRADITIONAL.txt\
+ tl.txt tl_PH.txt tzm_Latn_MA.txt tzm_MA.txt ug_Arab_CN.txt\
+ ug_CN.txt uz_AF.txt uz_Arab_AF.txt uz_Latn_UZ.txt uz_UZ.txt\
+ vai_LR.txt vai_Vaii_LR.txt zh_CN.txt zh_HK.txt zh_Hans_CN.txt\
+ zh_Hant_TW.txt zh_MO.txt zh_SG.txt zh_TW.txt
 
 
 # All aliases (to not be included under 'installed'), but not including root.
@@ -85,50 +85,50 @@ CURR_SOURCE = af.txt af_NA.txt agq.txt ak.txt\
  es_US.txt es_UY.txt es_VE.txt et.txt eu.txt\
  ewo.txt fa.txt fa_AF.txt ff.txt ff_GN.txt\
  ff_MR.txt fi.txt fil.txt fo.txt fr.txt\
- fr_BI.txt fr_CA.txt fr_CD.txt fr_CH.txt fr_DJ.txt\
- fr_DZ.txt fr_GN.txt fr_HT.txt fr_KM.txt fr_LU.txt\
- fr_MG.txt fr_MR.txt fr_MU.txt fr_RW.txt fr_SC.txt\
- fr_SY.txt fr_TN.txt fr_VU.txt fur.txt fy.txt\
- ga.txt gd.txt gl.txt gsw.txt gu.txt\
- guz.txt gv.txt ha.txt ha_Latn.txt ha_Latn_GH.txt\
- haw.txt he.txt hi.txt hr.txt hr_BA.txt\
- hsb.txt hu.txt hy.txt id.txt ig.txt\
- ii.txt is.txt it.txt it_CH.txt ja.txt\
- jgo.txt jmc.txt ka.txt kab.txt kam.txt\
- kde.txt kea.txt khq.txt ki.txt kk.txt\
- kk_Cyrl.txt kkj.txt kl.txt kln.txt km.txt\
- kn.txt ko.txt kok.txt ks.txt ks_Arab.txt\
- ksb.txt ksf.txt ksh.txt kw.txt ky.txt\
- ky_Cyrl.txt lag.txt lb.txt lg.txt lkt.txt\
- ln.txt ln_AO.txt lo.txt lt.txt lu.txt\
- luo.txt luy.txt lv.txt mas.txt mas_TZ.txt\
- mer.txt mfe.txt mg.txt mgh.txt mgo.txt\
- mk.txt ml.txt mn.txt mn_Cyrl.txt mr.txt\
- ms.txt ms_Latn.txt ms_Latn_BN.txt ms_Latn_SG.txt mt.txt\
- mua.txt my.txt naq.txt nb.txt nd.txt\
- ne.txt ne_IN.txt nl.txt nl_AW.txt nl_BQ.txt\
- nl_CW.txt nl_SR.txt nl_SX.txt nmg.txt nn.txt\
- nnh.txt nus.txt nyn.txt om.txt om_KE.txt\
- or.txt os.txt os_RU.txt pa.txt pa_Arab.txt\
- pa_Guru.txt pl.txt ps.txt pt.txt pt_AO.txt\
- pt_CV.txt pt_GW.txt pt_MO.txt pt_MZ.txt pt_PT.txt\
- pt_ST.txt pt_TL.txt qu.txt qu_BO.txt qu_EC.txt\
- rm.txt rn.txt ro.txt ro_MD.txt rof.txt\
- ru.txt ru_BY.txt ru_KG.txt ru_KZ.txt ru_MD.txt\
- rw.txt rwk.txt sah.txt saq.txt sbp.txt\
- se.txt se_SE.txt seh.txt ses.txt sg.txt\
- shi.txt shi_Latn.txt shi_Tfng.txt si.txt sk.txt\
- sl.txt smn.txt sn.txt so.txt so_DJ.txt\
- so_ET.txt so_KE.txt sq.txt sq_MK.txt sr.txt\
- sr_Cyrl.txt sr_Cyrl_BA.txt sr_Latn.txt sv.txt sw.txt\
- sw_UG.txt swc.txt ta.txt ta_LK.txt ta_MY.txt\
- ta_SG.txt te.txt teo.txt teo_KE.txt th.txt\
- ti.txt ti_ER.txt to.txt tr.txt twq.txt\
- tzm.txt tzm_Latn.txt ug.txt ug_Arab.txt uk.txt\
- ur.txt ur_IN.txt uz.txt uz_Arab.txt uz_Cyrl.txt\
- uz_Latn.txt vai.txt vai_Latn.txt vai_Vaii.txt vi.txt\
- vun.txt wae.txt xog.txt yav.txt yi.txt\
- yo.txt yo_BJ.txt zgh.txt zh.txt zh_Hans.txt\
- zh_Hans_HK.txt zh_Hans_MO.txt zh_Hans_SG.txt zh_Hant.txt zh_Hant_HK.txt\
- zh_Hant_MO.txt zu.txt
+ fr_BI.txt fr_CA.txt fr_CD.txt fr_DJ.txt fr_DZ.txt\
+ fr_GN.txt fr_HT.txt fr_KM.txt fr_LU.txt fr_MG.txt\
+ fr_MR.txt fr_MU.txt fr_RW.txt fr_SC.txt fr_SY.txt\
+ fr_TN.txt fr_VU.txt fur.txt fy.txt ga.txt\
+ gd.txt gl.txt gsw.txt gu.txt guz.txt\
+ gv.txt ha.txt ha_Latn.txt ha_Latn_GH.txt haw.txt\
+ he.txt hi.txt hr.txt hr_BA.txt hsb.txt\
+ hu.txt hy.txt id.txt ig.txt ii.txt\
+ is.txt it.txt ja.txt jgo.txt jmc.txt\
+ ka.txt kab.txt kam.txt kde.txt kea.txt\
+ khq.txt ki.txt kk.txt kk_Cyrl.txt kkj.txt\
+ kl.txt kln.txt km.txt kn.txt ko.txt\
+ kok.txt ks.txt ks_Arab.txt ksb.txt ksf.txt\
+ ksh.txt kw.txt ky.txt ky_Cyrl.txt lag.txt\
+ lb.txt lg.txt lkt.txt ln.txt ln_AO.txt\
+ lo.txt lt.txt lu.txt luo.txt luy.txt\
+ lv.txt mas.txt mas_TZ.txt mer.txt mfe.txt\
+ mg.txt mgh.txt mgo.txt mk.txt ml.txt\
+ mn.txt mn_Cyrl.txt mr.txt ms.txt ms_Latn.txt\
+ ms_Latn_BN.txt ms_Latn_SG.txt mt.txt mua.txt my.txt\
+ naq.txt nb.txt nd.txt ne.txt ne_IN.txt\
+ nl.txt nl_AW.txt nl_BQ.txt nl_CW.txt nl_SR.txt\
+ nl_SX.txt nmg.txt nn.txt nnh.txt nus.txt\
+ nyn.txt om.txt om_KE.txt or.txt os.txt\
+ os_RU.txt pa.txt pa_Arab.txt pa_Guru.txt pl.txt\
+ ps.txt pt.txt pt_AO.txt pt_CV.txt pt_GW.txt\
+ pt_MO.txt pt_MZ.txt pt_PT.txt pt_ST.txt pt_TL.txt\
+ qu.txt qu_BO.txt qu_EC.txt rm.txt rn.txt\
+ ro.txt ro_MD.txt rof.txt ru.txt ru_BY.txt\
+ ru_KG.txt ru_KZ.txt ru_MD.txt rw.txt rwk.txt\
+ sah.txt saq.txt sbp.txt se.txt se_SE.txt\
+ seh.txt ses.txt sg.txt shi.txt shi_Latn.txt\
+ shi_Tfng.txt si.txt sk.txt sl.txt smn.txt\
+ sn.txt so.txt so_DJ.txt so_ET.txt so_KE.txt\
+ sq.txt sq_MK.txt sr.txt sr_Cyrl.txt sr_Cyrl_BA.txt\
+ sr_Latn.txt sr_Latn_BA.txt sv.txt sw.txt sw_UG.txt\
+ swc.txt ta.txt ta_LK.txt ta_MY.txt ta_SG.txt\
+ te.txt teo.txt teo_KE.txt th.txt ti.txt\
+ ti_ER.txt to.txt tr.txt twq.txt tzm.txt\
+ tzm_Latn.txt ug.txt ug_Arab.txt uk.txt ur.txt\
+ ur_IN.txt uz.txt uz_Arab.txt uz_Cyrl.txt uz_Latn.txt\
+ vai.txt vai_Latn.txt vai_Vaii.txt vi.txt vun.txt\
+ wae.txt xog.txt yav.txt yi.txt yo.txt\
+ yo_BJ.txt zgh.txt zh.txt zh_Hans.txt zh_Hans_HK.txt\
+ zh_Hans_MO.txt zh_Hans_SG.txt zh_Hant.txt zh_Hant_HK.txt zh_Hant_MO.txt\
+ zu.txt
 
