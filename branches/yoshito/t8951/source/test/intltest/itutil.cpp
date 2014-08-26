02acd363d7d9fa807a3002f5228028ebae8ee458
yoshito <yoshito@251d0590-4201-4cf1-90de-194747b24ca1>
2014-08-26 23:47:30 +0000

    ticket:8951: Merged updates from trunk
    git-svn-id: http://source.icu-project.org/repos/icu/icu@36246 251d0590-4201-4cf1-90de-194747b24ca1

--- branches/yoshito/t8951/source/test/intltest/itutil.cpp
+++ branches/yoshito/t8951/source/test/intltest/itutil.cpp
@@ -33,8 +33,8 @@ extern IntlTest *createBytesTrieTest();
 static IntlTest *createLocalPointerTest();
 extern IntlTest *createUCharsTrieTest();
 static IntlTest *createEnumSetTest();
-extern IntlTest *createLRUCacheTest();
 extern IntlTest *createSimplePatternFormatterTest();
+extern IntlTest *createUnifiedCacheTest();
 
 #define CASE(id, test) case id:                               \
                           name = #test;                       \
@@ -98,18 +98,18 @@ void IntlTestUtilities::runIndexedTest( int32_t index, UBool exec, const char* &
             }
             break;
         case 20:
-            name = "LRUCacheTest";
-            if (exec) {
-                logln("TestSuite LRUCacheTest---"); logln();
-                LocalPointer<IntlTest> test(createLRUCacheTest());
-                callTest(*test, par);
-            }
-            break;
-        case 21:
             name = "SimplePatternFormatterTest";
             if (exec) {
                 logln("TestSuite SimplePatternFormatterTest---"); logln();
                 LocalPointer<IntlTest> test(createSimplePatternFormatterTest());
+                callTest(*test, par);
+            }
+            break;
+        case 21:
+            name = "UnifiedCacheTest";
+            if (exec) {
+                logln("TestSuite UnifiedCacheTest---"); logln();
+                LocalPointer<IntlTest> test(createUnifiedCacheTest());
                 callTest(*test, par);
             }
             break;
